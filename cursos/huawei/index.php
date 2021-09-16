e0d1ea6ea510b6e30b97d42b882f79bcea083a20
Ricardo MONLA <rmonla@gmail.com>
2021-09-15 21:22:17 -0300

    Actu_210915_2122

---
+++ cursos/huawei/index.php
@@ -0,0 +1,61 @@
+<?php
+	//include_once "../.rmGo.php";
+	//goDst("UTNLaRioja");
+
+
+	header('Content-Type: text/html; charset=utf-8');
+
+	$titu = "CURSOS.HUAWEI";
+	$desc = "ACADEMIA HUAWEI - UTNLaRioja 2021";
+
+	define('SITIO', 'http://www.frlr.utn.edu.ar/dtic/aulavirtual/');
+	define('URL', SITIO.'21/cursos/huawei');
+	
+	
+	$imag = SITIO."_imgs/logoUTNLR-Mini.png";
+
+	$time = 0;
+	$dest = URL;
+
+	$idG    = "614284b90b19a40d8ebd00d5";
+	$source = "https://view.genial.ly/$idG";
+
+	$htmRefresh = ($time) ? "<meta http-equiv='Refresh' content='$time;url=$dest'/>" : "" ;
+
+
+	//  $source = "./conferencias";
+
+	// http://www.frlr.utn.edu.ar/dtic/aulavirtual/21/posg/emduciii?01
+	
+	// $genialyID = "6027b4045884590d8d1a672f";
+	// $genialyURL = "https://view.genial.ly/" . $genialyID;
+
+							// <iframe frameborder="0" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="$genialyURL" type="text/html" allowscriptaccess="always" allowfullscreen="true" scrolling="yes" allownetworking="all"></iframe>
+
+	echo <<<HTML
+
+	<html>
+		<head>
+			<title>$titu</title>
+			<meta property="og:title"  content="$titu">
+			<meta name="description"   content="$desc">
+			<meta property="og:image"  content="$imag">
+			$htmRefresh
+		</head>
+		<body>
+			<center>
+				<p>
+					<div">
+						<div >
+							<iframe frameborder="0" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;" src="$source"></iframe>
+						</div>
+					</div>	
+				</p>
+			</center>
+		</body>
+	</html>		
+
+HTML;
+
+
+?>
--- pi22/index.php
+++ pi22/index.php
@@ -1,4 +1,4 @@
 <?php
-	include_once "../rmGo.php";
+	include_once "../.rmGo.php";
 	goDst("pi22");
 ?>
